370a54df96ac0fb1352e46846cf65eab66edcc76
李德堯 <ad841108@lideyaodembp.wifi.local.cmu.edu>
2021-10-21 11:03:20 -0400

    fix hw3

--- NoSQL/noSQL.py
+++ NoSQL/noSQL.py
@@ -5,77 +5,49 @@ s3 = boto3.resource('s3',
 	aws_access_key_id = 'AKIAU6PFDVGEZJCY6O2R',
 	aws_secret_access_key = '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')
 
-#s3.create_bucket(Bucket = 'hw3dli3', CreateBucketConfiguration = {'LocationConstraint': 'us-west-2'})
+s3.create_bucket(Bucket = 'hw3dli3', CreateBucketConfiguration = {'LocationConstraint': 'us-west-2'})
 
 dyndb = boto3.resource('dynamodb', region_name = 'us-west-2', aws_access_key_id = 'AKIAU6PFDVGEZJCY6O2R',
 	aws_secret_access_key = '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')
 
-# table = dyndb.create_table(
-#     TableName='hw3table2',
-#     KeySchema=[
-#         {
-#             'AttributeName': 'Id',
-#             'KeyType': 'HASH'  # Partition key
-#         },
-#         # {
-#         #     'AttributeName': 'Temp',
-#         #     'KeyType': 'RANGE'  # Sort key
-#         # },
-#         # {
-#         #     'AttributeName': 'Conductivity',
-#         #     'KeyType': 'RANGE'  # Partition key
-#         # },
-#         # {
-#         #     'AttributeName': 'Concentration',
-#         #     'KeyType': 'RANGE'  # Sort key
-#         # }
-#     ],
-#     AttributeDefinitions=[
-#         {
-#             'AttributeName': 'Id',
-#             'AttributeType': 'S'
-#         },
-#         # {
-#         #     'AttributeName': 'Temp',
-#         #     'AttributeType': 'N'
-#         # },
-#         # {
-#         #     'AttributeName': 'Conductivity',
-#         #     'AttributeType': 'N'
-#         # },
-#         # {
-#         #     'AttributeName': 'Concentration',
-#         #     'AttributeType': 'N'
-#         # }
+table = dyndb.create_table(
+    TableName='hw3table2',
+    KeySchema=[
+        {
+            'AttributeName': 'Id',
+            'KeyType': 'HASH'  # Partition key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'Id',
+            'AttributeType': 'S'
+        },
 
-#     ],
-#     ProvisionedThroughput={
-#         'ReadCapacityUnits': 100,
-#         'WriteCapacityUnits': 100
-#     }
-# )
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 100,
+        'WriteCapacityUnits': 100
+    }
+)
 
 table = dyndb.Table('hw3table2')
-# urlbase = 'https://hw3dli3.s3.us-west-2.amazonaws.com/'
-# with open('experiments.csv', 'r') as csvfile:
-#     csvf = csv.reader(csvfile, delimiter  = ',', quotechar = '|')
-#     count = 0
-#     print(csvf)
-#     for item in csvf:
-#         if(count == 0):
-#             count += 1
-#             continue
-#         body = open(item[4], 'rb')
-#         s3.Object('hw3dli3', item[4]).put(Body = body)
-#         md = s3.Object('hw3dli3', item[4]).Acl().put(ACL = 'public-read')
-#         url = urlbase + item[4]
-#         metadata_item = {'Id': item[0], 'Temp': item[1], 'Conductivity': item[2], 'Concentration': item[3], 'url':url}
-#         table.put_item(Item = metadata_item)
+urlbase = 'https://hw3dli3.s3.us-west-2.amazonaws.com/'
+with open('experiments.csv', 'r') as csvfile:
+    csvf = csv.reader(csvfile, delimiter  = ',', quotechar = '|')
+    count = 0
+    for item in csvf:
+        if(count == 0):
+            count += 1
+            continue
+        body = open(item[4], 'rb')
+        s3.Object('hw3dli3', item[4]).put(Body = body)
+        md = s3.Object('hw3dli3', item[4]).Acl().put(ACL = 'public-read')
+        url = urlbase + item[4]
+        metadata_item = {'Id': item[0], 'Temp': item[1], 'Conductivity': item[2], 'Concentration': item[3], 'url':url}
+        table.put_item(Item = metadata_item)
 
 for i in range(1, 4):
     print(table.get_item(Key = {'Id': str(i)})['Item'])
 
-
-
-# table.meta.client.get_waiter('table_exists').wait(TableName = 'DataTable')
 
